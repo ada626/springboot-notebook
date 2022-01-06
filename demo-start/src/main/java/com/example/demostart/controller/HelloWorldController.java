3365f15ee0075b089396aa5a8ac1fae1cb6799f6
ada626 <ada.yxin@gmail.com>
2022-01-06 19:31:05 +0800

    second project

--- demo-start/src/main/java/com/example/demostart/controller/HelloWorldController.java
+++ demo-start/src/main/java/com/example/demostart/controller/HelloWorldController.java
@@ -1,2 +1,13 @@
-package com.example.demostart.controller;public class HelloWorldController {
+package com.example.demostart.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping({"/test"})
+public class HelloWorldController {
+    @GetMapping("hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
 }
