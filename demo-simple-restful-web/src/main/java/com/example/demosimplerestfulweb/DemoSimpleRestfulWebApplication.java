e5049239fbf9e7349c5b32661fde6b462f215a5a
ada626 <ada.yxin@gmail.com>
2022-01-06 19:38:35 +0800

    the 2nd version of restful project created

---
+++ demo-simple-restful-web/src/main/java/com/example/demosimplerestfulweb/DemoSimpleRestfulWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosimplerestfulweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSimpleRestfulWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSimpleRestfulWebApplication.class, args);
+    }
+
+}
---
+++ demo-simple-restful-web/src/test/java/com/example/demosimplerestfulweb/DemoSimpleRestfulWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosimplerestfulweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSimpleRestfulWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
