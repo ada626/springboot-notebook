33f903ab329cf59601590b8bcc60392b3986f626
ada626 <ada.yxin@gmail.com>
2022-01-07 08:18:51 +0800

    first commit

---
+++ first-completed-project-mall/src/main/java/com/example/firstcompletedprojectmall/FirstCompletedProjectMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstcompletedprojectmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCompletedProjectMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstCompletedProjectMallApplication.class, args);
+    }
+
+}
---
+++ first-completed-project-mall/src/test/java/com/example/firstcompletedprojectmall/FirstCompletedProjectMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstcompletedprojectmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCompletedProjectMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
