102d441c71f8ac884b9a8e3c9f074adaacbc0842
ada626 <ada.yxin@gmail.com>
2022-01-08 22:09:47 +0800

    password security--MD5+SALT

--- first-completed-project-mall/src/main/java/com/imooc/mall/service/impl/UserServiceImpl.java
+++ first-completed-project-mall/src/main/java/com/imooc/mall/service/impl/UserServiceImpl.java
@@ -4,8 +4,11 @@ import com.imooc.mall.exception.MallExceptionEnum;
 import com.imooc.mall.model.dao.UserMapper;
 import com.imooc.mall.model.pojo.User;
 import com.imooc.mall.service.UserService;
+import com.imooc.mall.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.security.NoSuchAlgorithmException;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -28,7 +31,11 @@ public class UserServiceImpl implements UserService {
         //写到数据库
         User user = new User();
         user.setUsername(username);
-        user.setPassword(password);
+        try {
+            user.setPassword(MD5Utils.getMD5Str(password));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
         int count = userMapper.insertSelective(user);
         if(count==0){
             throw new MallException(MallExceptionEnum.INSERT_FAILED);
